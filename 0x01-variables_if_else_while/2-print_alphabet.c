8378a7039e49c7aef97f517a838ebc1e980cd5f8
GideonAg <gideonagbosu72@gmail.com>
2022-10-16 07:59:10 -0700

    Added 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-#include <string.h>
 
 int main(void)
 {
-char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
-for (int i = 0; i < strlen(alphabet); i++)
+char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+i = 0;
+for (; i < 26;)
 {
 char ch = alphabet[i];
+i++;
 putchar(ch);
 }
 putchar('\n');
