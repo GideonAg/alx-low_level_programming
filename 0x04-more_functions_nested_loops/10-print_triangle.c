ae2b82050bd0b5090802bea00daca22d8a75905d
GideonAg <gideonagbosu72@gmail.com>
2022-10-21 22:28:57 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - star of program
+ * @size: triangel size
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int row, hashes, spaces;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (row = 1; <= size; row++)
+		{
+			for (spaces = size - row; spaces >= q; spaces--)
+			{
+				_putchar(' ');
+			}
+			for (hashes = 1; hashes <= row; hashes++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
