831d9ce953163dfb5597108546509a290f5b5baa
GideonAg <gideonagbosu72@gmail.com>
2022-10-21 22:19:37 -0700

    modified

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,11 +16,11 @@ int main(void)
 			printf("FizzBuzz");
 		else if (i % 3 == 0)
 			printf("Fizz");
-		else if (i % 5 ==0)
+		else if (i % 5 == 0)
 			printf("Buzz");
 		else
 			printf("%i", i);
-		if <i < 100)
+		if (i < 100)
 			printf(" ");
 	}
 	printf("\n");
