06ab6c44cac68e27fff2da8b7d17fb14ccf65e78
GideonAg <gideonagbosu72@gmail.com>
2022-10-24 08:23:52 -0700

    Added 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - start of program
+ * @s: parameter
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int len = strlen(s) - 1;
+	int i;
+
+	for (i = len; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
