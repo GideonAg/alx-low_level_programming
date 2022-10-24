5aa135a509b4e2d22fd75791545f7480d4b0f7d5
GideonAg <gideonagbosu72@gmail.com>
2022-10-24 06:02:38 -0700

    Added 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - start of program
+ * @a: parameter
+ * @b: parameter
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *pA = &b;
+	*b = *a;
+	*a = *pA;
+}
