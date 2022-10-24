060a7faaa901fc2c1acb6b1c983afaf3f95e458a
GideonAg <gideonagbosu72@gmail.com>
2022-10-24 16:31:45 -0700

    Added 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcpy - start of program
+ * @dest: parameter
+ * @src: parameter
+ * Return: value of chars
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *pD = NULL;
+
+	strcpy(*pD, *src);
+}
