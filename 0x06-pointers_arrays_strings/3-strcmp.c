ca5a0cba88aeedcdefd3ba7f336388f73b7c4429
GideonAg <gideonagbosu72@gmail.com>
2022-10-26 07:21:18 -0700

    Added 2-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - start of program
+ * @s1: parameter
+ * @s2: parameter
+ * Return: return 0 if strings compared are the same, else something else
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
