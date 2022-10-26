893bb2e3d0f9453f5f4c6d52981e47ce16ebc3a5
GideonAg <gideonagbosu72@gmail.com>
2022-10-26 06:51:03 -0700

    Added 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - start of program
+ * @dest: parameter
+ * @src: parameter
+ * Return: a pointer
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
