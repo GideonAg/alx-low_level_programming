74099f282af9aaeeaae4cd19804823c820160d93
GideonAg <gideonagbosu72@gmail.com>
2022-10-26 09:44:34 -0700

    Added 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main."
+#include <string.h>
+
+/**
+ * string_toupper - start of program
+ * Retrun: Always 0
+ */
+
+char *string_toupper(char *)
+{
+	int len = strlen(*);
+	int i;
+
+	for (i = 0; i < len; i++)
+	{
+		if (islow(*[i]))
+			toupper(*[i]);
+	}
+}
