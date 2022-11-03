c6ba18d78e4fd00b9d1ed13b9f7fcbdf73436d03
GideonAg <gideonagbosu72@gmail.com>
2022-11-02 21:16:54 -0700

    Added a file

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _print_rev_recursion - start
+ * @s: parameter
+ * Return: Always 0
+ */
+
+_print_rev_recursion(char *s)
+{
+	int len = strlen(*s) - 1;
+	int i;
+
+	for (i = len; i >= 0; i--)
+		_putchar(s[i]);
+}
