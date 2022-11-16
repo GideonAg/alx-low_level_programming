d96d8979133dc052ad31d880a4623e15e54f4b9b
GideonAg <gideonagbosu72@gmail.com>
2022-11-16 05:52:55 -0800

    Added +, -, *, /, and % function

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - sum a and b
+ * @a: parameter
+ * @b: parameter
+ * Return: integer
+ */
+
+int op_add(int a, int b)
+{
+	return ((a) + (b));
+}
+
+/**
+ * op_sub - subtract a and b
+ * @a: parameter
+ * @b: parameter
+ * Return: integer
+ */
+
+int op_sub(int a, int b)
+{
+	return ((a) - (b));
+}
+
+/**
+ * op_mul - multiply a and b
+ * @a: parameter
+ * @b: parameter
+ * Return: integer
+ */
+
+int op_mul(int a, int b)
+{
+	return ((a) * (b));
+}
+
+/**
+ * op_div - divide a and b
+ * @a: parameter
+ * @b: parameter
+ * Return: integer
+ */
+
+int op_div(int a, int b)
+{
+	return ((a) / (b));
+}
+
+/**
+ * op_mod - remainder of the division of a by b
+ * @a: parameter
+ * @b: parameter
+ * Return: integer
+ */
+
+int op_mod(int a, int b)
+{
+	return ((a) % (b));
+}
