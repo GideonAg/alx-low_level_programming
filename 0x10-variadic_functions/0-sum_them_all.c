8da9fb8ff480fb082963d20ef9d3d54e503a87f6
GideonAg <gideonagbosu72@gmail.com>
2022-11-17 22:38:52 -0800

    commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include  "variadic_functions.h"
+
+/**
+ * sum_them_all - start
+ * @n: parameter
+ * Return: integer
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	int i;
+	va_list arg;
+
+	va_start(arg, n);
+
+	if (n != 0)
+	{
+		for (i = 0; i < n; i++)
+			sum += va_arg(arg, int);
+		return (sum);
+	}
+	va_end(arg);
+	return (0);
+}
