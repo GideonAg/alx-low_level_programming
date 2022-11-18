702723beb190e71572f344581f521b66610c8cae
GideonAg <gideonagbosu72@gmail.com>
2022-11-17 23:05:07 -0800

    commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -19,11 +19,8 @@ int sum_them_all(const unsigned int n, ...)
 	if (n == 0)
 		return (0);
 
-	if (n != 0)
-	{
-		for (i = 0; i < n; i++)
-			sum += va_arg(arg, int);
-		return (sum);
-	}
+	for (i = 0; i < n; i++)
+		sum += va_arg(arg, int);
 	va_end(arg);
+	return (sum);
 }
