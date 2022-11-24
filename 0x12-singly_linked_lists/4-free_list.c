a11c4a24048838247ceb509d14be2c5bad1c8648
GideonAg <gideonagbosu72@gmail.com>
2022-11-24 15:23:04 -0800

    Added free_list file

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees allocated memory space
+ * @head: pointer to allocated memory space
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	free(head);
+}
