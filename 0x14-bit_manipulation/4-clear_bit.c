23bb36dcbf03b61e86164c5b975c57524454b9f8
GideonAg <gideonagbosu72@gmail.com>
2022-12-03 02:15:01 -0800

    commit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,23 @@
+#include "mainh"
+
+/**
+ * clear_bit - start
+ * @n: parameter
+ * @index: parameter
+ * Return: 1, else -1 if an error occurred
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int a;
+
+	if (index > 63)
+		return (-1);
+
+	a = 1 << index;
+
+	if (*n & a)
+		*n ^= m;
+
+	return (1);
+}
