f7f01391383bc06adb0e52c76ff5b7d95f07bb82
GideonAg <gideonagbosu72@gmail.com>
2022-12-02 23:32:12 -0800

    commit

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - start
+ * @head: parameter
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+	listint *n;
+
+	if (head != NULL)
+	{
+		n = *head;
+
+		while ((temp = n) != NULL)
+		{
+			n = n->next;
+			free(temp);
+		}
+		head = NULL;
+	}
+}
