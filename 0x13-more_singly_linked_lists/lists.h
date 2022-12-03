3005ecf56b90a313ca64f4fada3e431ac87676c1
GideonAg <gideonagbosu72@gmail.com>
2022-12-02 22:48:10 -0800

    commit

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,36 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+/**
+ * struct listint_s - A typedef struct of listint_s
+ * @n: integer type
+ * @next: pointer to next node
+ */
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+/**
+ * struct listp_s = A typedef struct of listp_s
+ * @p: void pointer
+ * @next: pointer to next node
+ */
+
+typedef struct listp_s
+{
+	void *p;
+	struct listp_s *next;
+} listp_t;
+
+size_t print_listint(const listint_t *h);
+
+
+#endif
