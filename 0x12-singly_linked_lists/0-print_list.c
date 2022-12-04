ad12c482e33ba45d97185c6b79edb50378e9dde6
GideonAg <gideonagbosu72@gmail.com>
2022-12-03 23:02:34 -0800

    commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,28 +1,25 @@
-#include <stdio.h>
-#include "list.h"
+#include "lists.h"
 
 /**
- * print_list - start
+ * print_list - print a list on nodes
  * @h: parameter
- * Return: number of nodes
+ * Return: the number of nodes
  */
 
 size_t print_list(const list_t *h)
 {
-	int count = 0;
+	size_t nodes = 0;
 
 	while (h)
 	{
 		if (h->str == NULL)
-		{
 			printf("[0] (nil)\n");
-		}
 		else
-		{
 			printf("[%d] %s\n", h->len, h->str);
-		}
-		count++;
+
+		nodes++;
 		h = h->next;
 	}
-	return (count);
+
+	return (nodes);
 }
